c4cf926b4319604d43edf997d7f67992fc710988
Tristal Li <yanlinlily@126.com>
2021-06-30 13:39:42 +0800

    add index page

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ import os
 import sys
 import click
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
 from flask_restful import Resource, Api
 import datetime as dt
-from flask import Flask
+from flask import Flask, jsonify, render_template, request, url_for, redirect, flash
 
 from bsv.downloader import *
 
@@ -35,6 +36,9 @@ class Holdings(db.Model):
     member = db.Column(db.String(4))
     value = db.Column(db.Integer)
     change = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Holding {0}: tday {1}, exchange {2}>'.format(self.id, self.tday, self.exchange)
 
 def get_next_date(date, timedelta=1, timeformat='%Y%m%d'):
     """
@@ -74,6 +78,7 @@ def update_data(start_date, end_date, exchange):
         if not os.path.exists(dce_zip):
             os.mkdir(dce_zip)
     _func = globals()['_'.join(['hp', exchange])]
+    db.drop_all()
     db.create_all()
 
     while start_date <= end_date:
@@ -82,25 +87,80 @@ def update_data(start_date, end_date, exchange):
         else:
             _generator = _func(start_date)
         for i in _generator:
-            db.session.add(Holdings(
-                tday=i[0],
-                exchange=i[1],
-                code=i[2],
-                product=i[3],
-                contract=i[4],
-                symbol=i[5],
-                rank=i[6],
-                member=i[7],
-                value=i[8],
-                change=i[9]
-            ))
+            if not Holdings.query.filter_by(tday=i[0]).filter_by(exchange=exchange.upper()).first():
+                db.session.add(Holdings(
+                    tday=i[0],
+                    exchange=i[1],
+                    code=i[2],
+                    product=i[3],
+                    contract=i[4],
+                    symbol=i[5],
+                    rank=i[6],
+                    member=i[7],
+                    value=i[8],
+                    change=i[9]
+                ))
         db.session.commit()
         start_date = get_next_date(start_date)
 
-if __name__ == "__main__":
-    update_data()
+class RequestExchange(Resource):
+
+    def get(self):
+        ex = list(Holdings.query.with_entities(Holdings.exchange).distinct().all()[0])
+        start_date = db.session.query(func.min(Holdings.tday)).all()[0][0]
+        end_date = db.session.query(func.max(Holdings.tday)).all()[0][0]
+        return jsonify(status_code = 200,
+                       msg = 'Success',
+                       data = {'exchange': ex,
+                               'start_date': start_date,
+                               'end_date': end_date})
 
 
+@app.route("/", methods = ["GET", "POST"])
+def index():
+    holdings = Holdings.query.all()
+    if request.method == 'POST':
+        start_date = request.form.get('start_date')
+        end_date = request.form.get('end_date')
+        exchange = request.form.get('exchange')
+
+        exchange = exchange.lower()
+        if exchange == 'dce':
+            if not os.path.exists(dce_dir):
+                os.mkdir(dce_dir)
+            if not os.path.exists(dce_zip):
+                os.mkdir(dce_zip)
+        _func = globals()['_'.join(['hp', exchange])]
+        db.drop_all()
+        db.create_all()
+
+        if not end_date:
+            end_date = dt.datetime.today().strftime("%Y%m%d")
+
+        while start_date <= end_date:
+            if exchange == 'dce':
+                _generator = _func(start_date, dce_dir, dce_zip)
+            else:
+                _generator = _func(start_date)
+            for i in _generator:
+                db.session.add(Holdings(
+                    tday=i[0],
+                    exchange=i[1],
+                    code=i[2],
+                    product=i[3],
+                    contract=i[4],
+                    symbol=i[5],
+                    rank=i[6],
+                    member=i[7],
+                    value=i[8],
+                    change=i[9]
+                ))
+            db.session.commit()
+            start_date = get_next_date(start_date)
+
+        holdings = Holdings.query.all()
+        return redirect(url_for('index'))
+    return render_template("base.html", holdings = holdings)
 
 
 
